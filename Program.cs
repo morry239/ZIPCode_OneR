5635b2a2ac2e00b5c98ba32a18c2906e04f5b4cd
morry239 <73179728+morry239@users.noreply.github.com>
2023-03-13 13:38:10 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,10 +14,10 @@ class Program
             
         }*/
             //oneR.Build(new StreamReader(args[0], Encoding.Default));
-            /*string filePath = @"/Users/mamikawamura/Desktop/geo-data.csv";
+            /*string filePath = @"/Users//geo-data.csv";
             using FileStream fs = File.Create(filePath); //applied FileStream*/
-            oneR.Build(new StreamReader("/Users/mamikawamura/Desktop/geo-data.csv"));
-            Console.WriteLine("\nGültigen Wert zur Vorhersage eingeben");
+            oneR.Build(new StreamReader("/Users//geo-data.csv"));
+            Console.WriteLine("\nType the valid value for the prediction");
 
 
         string value = Console.ReadLine();
